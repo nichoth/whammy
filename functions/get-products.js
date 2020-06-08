c27510645df4258294c087109088a37ec7175853
nichoth <nichoth@gmail.com>
2020-06-07 21:28:03 -0700

    get products

--- functions/get-products.js
+++ functions/get-products.js
@@ -1,9 +1,28 @@
-const products = require('./data/products.json');
+// const products = require('./data/products.json');
+// require('dotenv').config()
 
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(products),
-  };
-};
+// [https://stripe.com/docs/api/products/list](List all products)
+exports.handler = function (ev, ctx, cb) {
+    var stripe = require('stripe')(process.env.STRIPE_SECRET);
+
+    stripe.products.list({ limit: 3 }, function (err, products) {
+        if (err) return cb(null, {
+            statusCode: 400,
+            body: JSON.stringify(err)
+        })
+
+        cb(null, {
+            statusCode: 200,
+            body: JSON.stringify(products)
+        })
+    });
+}
+
+
+// exports.handler = async () => {
+//   // return {
+//   //   statusCode: 200,
+//   //   body: JSON.stringify(products),
+//   // };
+// }
 
