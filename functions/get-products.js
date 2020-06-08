d72df3b2873015d49dbafc32abda5d6ec4d169e9
nichoth <nichoth@gmail.com>
2020-06-08 09:20:34 -0700

    get prices

--- functions/get-products.js
+++ functions/get-products.js
@@ -4,20 +4,39 @@
 // [https://stripe.com/docs/api/products/list](List all products)
 exports.handler = function (ev, ctx, cb) {
     var stripe = require('stripe')(process.env.STRIPE_SECRET);
+    var _products, _prices
 
     stripe.products.list({ limit: 3 }, function (err, products) {
-        console.log('woooooo', err, products)
-
         if (err) return cb(null, {
             statusCode: 400,
             body: JSON.stringify(err)
         })
 
+        _products = products
+        allDone()
+    });
+
+    stripe.prices.list({}, function (err, prices) {
+        if (err) return cb(null, {
+            statusCode: 400,
+            body: JSON.stringify(err)
+        })
+
+        _prices = prices
+        allDone()
+    })
+
+    function allDone () {
+        if (!_products || !_prices) return
+
         cb(null, {
             statusCode: 200,
-            body: JSON.stringify(products)
+            body: JSON.stringify({
+                products: _products,
+                prices: _prices
+            })
         })
-    });
+    }
 }
 
 
