882f523c690f8564dccd62c305ee6d25c3d189ba
nichoth <nichoth@gmail.com>
2020-06-24 12:57:57 -0700

    separate html for single product

--- src/js/load-products.js
+++ src/js/load-products.js
@@ -1,29 +1,6 @@
 // import { handleFormSubmission } from './stripe-purchase.js';
 // var stripeKey = 'pk_test_51GrU9fGmvbUUvDLHCSTZ5S1cvBn6pKJdo4fBrit12yFXcV8igIQ2ACaNGV2SkHXN4jiklVSRkXOkQdpKLfPh3MKo00i1PbHHID'
 
-function createSingleProduct (item) {
-    const template = document.querySelector('#single-product');
-    const product = template.content.cloneNode(true);
-
-    // todo -- put content in the template
-    product.querySelector('h2').innerText = item.name;
-    product.querySelector('.description').innerText = item.description;
-    // product.querySelector('[name=sku]').value = item.id;
-    // product.querySelector('.price').innerText = item.price;
-    product.querySelector('.price').innerText = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-    }).format((item.price || 0).toFixed(2));
-
-    const img = product.querySelector('img');
-    // img.src = item.images[0];
-    var imgSrc = item.pic.split('/')
-    img.src = '/' + imgSrc[1] + '/' + imgSrc[2]
-    img.alt = item.name;
-
-    return product
-}
-
 function createProductFromTemplate (item) {
     // console.log('item', item)
     // const stripe = Stripe(stripeKey);
@@ -70,7 +47,6 @@ export async function loadProducts() {
 
     // const container = document.querySelector('.products');
     const container = document.querySelector('.products');
-    document.querySelector('.single-product-container').remove()
 
     console.log('products', res)
 
@@ -80,32 +56,5 @@ export async function loadProducts() {
     })
 }
 
-async function loadSingleProduct (slug) {
-    const res = await fetch('/.netlify/functions/get-single-product', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ slug })
-    })
-        .then((res) => res.json())
-        .catch((err) => console.error(err));
 
-    console.log('woooooo', res)
-
-    var container = document.querySelector('.single-product-container')
-    document.querySelector('.products').remove()
-    var content = createSingleProduct(res)
-    container.appendChild(content)
-}
-
-var href = window.location.href
-var segments = href.split('/')
-var path = segments[3]
-if (path.length === 0) {
-    console.log('all products')
-    loadProducts()
-} else {
-    console.log('single product', path)
-    loadSingleProduct(path)
-}
+loadProducts()
---
+++ src/js/load-single-product.js
@@ -0,0 +1,49 @@
+// import { handleFormSubmission } from './stripe-purchase.js';
+// var stripeKey = 'pk_test_51GrU9fGmvbUUvDLHCSTZ5S1cvBn6pKJdo4fBrit12yFXcV8igIQ2ACaNGV2SkHXN4jiklVSRkXOkQdpKLfPh3MKo00i1PbHHID'
+
+function createSingleProduct (item) {
+    const template = document.querySelector('#single-product');
+    const product = template.content.cloneNode(true);
+
+    // todo -- put content in the template
+    product.querySelector('h2').innerText = item.name;
+    product.querySelector('.description').innerText = item.description;
+    // product.querySelector('[name=sku]').value = item.id;
+    // product.querySelector('.price').innerText = item.price;
+    product.querySelector('.price').innerText = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format((item.price || 0).toFixed(2));
+
+    const img = product.querySelector('img');
+    // img.src = item.images[0];
+    var imgSrc = item.pic.split('/')
+    img.src = '/' + imgSrc[1] + '/' + imgSrc[2]
+    img.alt = item.name;
+
+    return product
+}
+
+async function loadSingleProduct (slug) {
+    const res = await fetch('/.netlify/functions/get-single-product', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ slug })
+    })
+        .then((res) => res.json())
+        .catch((err) => console.error(err));
+
+    console.log('woooooo', res)
+
+    var container = document.querySelector('.single-product-container')
+    var content = createSingleProduct(res)
+    container.appendChild(content)
+}
+
+var href = window.location.href
+var segments = href.split('/')
+var path = segments[3]
+console.log('single product', path)
+loadSingleProduct(path)
