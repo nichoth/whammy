a74adc4e07a4bb195cd908473819fb76628e87e3
nichoth <nichoth@gmail.com>
2020-06-25 20:44:01 -0700

    stuff

--- src/js/stripe-purchase.js
+++ src/js/stripe-purchase.js
@@ -1,9 +1,10 @@
-
 export async function handleFormSubmission (ev, card, stripe, clientSecret) {
     ev.preventDefault();
     var els = ev.target.elements
     console.log('form submit', ev, els)
     console.log('card', card)
+
+    // https://stripe.com/docs/payments/accept-a-payment#web
 
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
@@ -15,9 +16,21 @@ export async function handleFormSubmission (ev, card, stripe, clientSecret) {
     })
         .then(function (res) {
             console.log('res here', res)
+
+            if (res.error) {
+                // todo -- show message to the user
+                return console.log('errrrp', res.error)
+            }
+
+            // server-side -- create order in DB then do stripe payment
+            // or via webhook for success
+            // * shipping address
+
+            // Set up a webhook or plugin to listen for the
+            // payment_intent.succeeded event that handles any business
+            // critical post-payment actions.
         })
         .catch(function (err) {
             console.log('oh no', err)
         })
 }
-
