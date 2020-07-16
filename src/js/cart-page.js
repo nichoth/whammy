3ed0000cd4533c52fef1e6f927252f7f332f00a3
nichoth <nichoth@gmail.com>
2020-07-15 17:19:49 -0700

    start map fn

--- src/js/cart-page.js
+++ src/js/cart-page.js
@@ -3,4 +3,8 @@ import Cart from '@nichoth/shopping-cart'
 
 var cartContainer = document.getElementById('cart-page-container')
 var cart = new Cart()
-cart.createPage(cartContainer)
+cart.createPage(cartContainer, mapper)
+
+function mapper (html, product) {
+    return html`<li>product ${product}<//>`
+}
