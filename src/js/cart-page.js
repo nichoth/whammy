68af62b57ef20a04fc4b62aafa588d26d80d1523
nichoth <nichoth@gmail.com>
2020-07-15 17:40:56 -0700

    cart index

--- src/js/cart-page.js
+++ src/js/cart-page.js
@@ -4,7 +4,7 @@ var cartContainer = document.getElementById('cart-page-container')
 var cart = new Cart()
 cart.createPage(cartContainer, mapper)
 
-function mapper (html, product) {
-    console.log('in map', product)
+function mapper (html, product, i) {
+    console.log('in map', product, i)
     return html`<li>product: ${product.name}<//>`
 }
