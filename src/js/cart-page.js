7fb5e6ddd008e58470e3e3134e56bcd8f40fb64d
nichoth <nichoth@gmail.com>
2020-07-16 22:07:24 -0700

    better cart page

--- src/js/cart-page.js
+++ src/js/cart-page.js
@@ -11,9 +11,19 @@ cart.createPage(cartContainer, mapper)
 
 function mapper (html, product, i) {
     console.log('in map', product, i)
+
+    function Quantity (props) {
+        var { item } = props
+        if (item.quantity > 1) {
+            return html`<input type="number" max=${item.quantity} value=1 />`
+        }
+        return html`<span class="quantity">Qty 1</span>`
+    }
+
     return html`<span class="item-controls">
         <img src="${util.getImgUrl(product)}" alt=${product.name} />
         <h2><a href=${product.slug}>${product.name}</a></h2>
+        <${Quantity} item=${product} />
         <span class="price">$${product.price}</span>
     </span>`
 }
@@ -22,11 +32,10 @@ var el = document.getElementById('cart-totals')
 var subTotal = cart.products().reduce(function (acc, { price }) {
     return acc + price
 }, 0)
-var div = makeDiv('subtotal $' + subTotal)
-var ship = makeDiv('shipping $0')
-el.appendChild(div)
-el.appendChild(ship)
-el.appendChild(makeDiv('total $' + subTotal))
+// var ship = makeDiv('shipping $0')
+el.appendChild(makeDiv('subtotal $' + subTotal))
+// el.appendChild(ship)
+// el.appendChild(makeDiv('total $' + subTotal))
 
 function makeDiv (text) {
     var div = document.createElement('div')
