9465ac2b76c1b0153fff1aed060fd3a8be03f383
nichoth <nichoth@gmail.com>
2020-07-16 20:19:41 -0700

    placeholders for totals

--- src/js/cart-page.js
+++ src/js/cart-page.js
@@ -6,15 +6,30 @@ var cartContainer = document.getElementById('cart-page-container')
 var cart = new Cart({ key: KEY })
 cart.createPage(cartContainer, mapper)
 
-var iconContainer = document.getElementById('cart-icon-container')
-cart.createIcon(iconContainer)
+// var iconContainer = document.getElementById('cart-icon-container')
+// cart.createIcon(iconContainer)
 
 function mapper (html, product, i) {
     console.log('in map', product, i)
-    return html`
-        <span>
-            <img src="${util.getImgUrl(product)}" alt=${product.name} />
-            product: ${product.name || 'no name'}
-        </span>
-    `
+    return html`<span class="item-controls">
+        <img src="${util.getImgUrl(product)}" alt=${product.name} />
+        <h2><a href=${product.slug}>${product.name}</a></h2>
+        <span class="price">$${product.price}</span>
+    </span>`
+}
+
+var el = document.getElementById('cart-totals')
+var subTotal = cart.products().reduce(function (acc, { price }) {
+    return acc + price
+}, 0)
+var div = makeDiv('subtotal $' + subTotal)
+var ship = makeDiv('shipping $0')
+el.appendChild(div)
+el.appendChild(ship)
+el.appendChild(makeDiv('total $' + subTotal))
+
+function makeDiv (text) {
+    var div = document.createElement('div')
+    div.appendChild(document.createTextNode(text))
+    return div
 }
