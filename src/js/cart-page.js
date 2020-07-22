a7a93af09f58ba979dc681e6d4667828705c5b95
nichoth <nichoth@gmail.com>
2020-07-22 12:30:38 -0700

    update cart on remove

--- src/js/cart-page.js
+++ src/js/cart-page.js
@@ -1,6 +1,7 @@
 import Cart from '@nichoth/shopping-cart'
 import KEY from './KEY'
 import util from './util'
+import EVENTS from '@nichoth/shopping-cart/src/EVENTS'
 
 var cartContainer = document.getElementById('cart-page-container')
 var cart = new Cart({ key: KEY })
@@ -28,30 +29,58 @@ function mapper (html, product, i) {
     </span>`
 }
 
-var el = document.getElementById('cart-totals')
-var subTotal = cart.products().reduce(function (acc, { price }) {
-    return acc + price
-}, 0)
-function getShipping (l) {
-    if (l === 0) return 0
-    if (l === 1) return 3
-    if (l === 2) return 4
-    if (l >= 3 && l <= 8) return 5
-    if (l > 8) return 6
+cart.on(EVENTS.cart.remove, function (i) {
+    renderTotals(document.getElementById('cart-totals'), cart)
+    renderControls(document.querySelector('.cart-controls'), cart)
+})
+
+function renderTotals (el, cart) {
+    // var el = document.getElementById('cart-totals')
+    el.innerHTML = ''
+    if (cart.products().length === 0) return
+    var subTotal = cart.products().reduce(function (acc, { price }) {
+        return acc + price
+    }, 0)
+    function getShipping (l) {
+        if (l === 0) return 0
+        if (l === 1) return 3
+        if (l === 2) return 4
+        if (l >= 3 && l <= 8) return 5
+        if (l > 8) return 6
+    }
+
+    // cart.state(function onChange (state) {
+    //     // re-render the stuff
+    // })
+
+    // var ship = makeDiv('shipping $0')
+    el.appendChild(makeDiv('subtotal $' + subTotal))
+    var shippingCost = getShipping(cart.products().length)
+    el.appendChild(makeDiv('shipping $' + shippingCost))
+    el.appendChild(makeDiv('total $' + (subTotal + shippingCost)))
 }
-
-// cart.state(function onChange (state) {
-//     // re-render the stuff
-// })
-
-// var ship = makeDiv('shipping $0')
-el.appendChild(makeDiv('subtotal $' + subTotal))
-var shippingCost = getShipping(cart.products().length)
-el.appendChild(makeDiv('shipping $' + shippingCost))
-el.appendChild(makeDiv('total $' + (subTotal + shippingCost)))
 
 function makeDiv (text) {
     var div = document.createElement('div')
     div.appendChild(document.createTextNode(text))
     return div
 }
+
+function renderControls (el, cart) {
+    console.log('length', cart.products().length)
+    if (cart.products().length < 1) {
+        // remove the link
+        var link = el.querySelector('a.buy')
+        if (link) return link.remove()
+    }
+
+    var link = document.createElement('a')
+    link.href = '/buy-things'
+    link.classList.add('buy')
+    link.appendChild(document.createTextNode('buy things'))
+    el.appendChild(link)
+}
+
+var el = document.getElementById('cart-totals')
+renderTotals(el, cart)
+renderControls(document.querySelector('.cart-controls'), cart)
