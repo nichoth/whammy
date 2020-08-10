02c60060abdb86d6abb84cc8fe050fc0003d3605
nichoth <nichoth@gmail.com>
2020-08-10 12:18:51 -0700

    parse query string

--- src/js/success.js
+++ src/js/success.js
@@ -1,8 +1,12 @@
 import { html } from 'htm/preact'
 import { render } from 'preact'
+const queryString = require('query-string')
 
 function Success (props) {
     return html`<div>fooooo</div>`
 }
 
 render(html`<${Success} />`, document.getElementById('content'))
+
+var q = queryString.parse(window.location.search)
+console.log('q', q)
