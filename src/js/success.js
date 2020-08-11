aa60c82e2455979eb1c2f27456b9b0c3e9cb59c2
nichoth <nichoth@gmail.com>
2020-08-10 17:41:19 -0700

    better receipt style

--- src/js/success.js
+++ src/js/success.js
@@ -1,3 +1,4 @@
+var price = require('./price')
 import { html } from 'htm/preact'
 import { render } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
@@ -9,6 +10,7 @@ var { order_id } = qs
 function Success (props) {
     var { orderId } = props
     var [order, setOrder] = useState(null)
+
     useEffect(() => {
         // document.title = props.title;
         fetch('/.netlify/functions/get-order', {
@@ -27,7 +29,15 @@ function Success (props) {
     }, [props.orderId])
 
     console.log('props and state', props, order)
-    return html`<div>fooooo</div>`
+
+    if (!order) return null
+
+    return html`<ul class="receipt">
+        ${order.line_items.map(item => {
+            return html`<li><span>${item.name}</span> <span>${price.format(
+            item.base_price_money.amount)}</span></li>`
+        })}
+    </ul>`
 }
 
 render(html`<${Success} orderId=${order_id} />`,
